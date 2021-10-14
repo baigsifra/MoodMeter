123a74556ff9327e9739bfb41326517bb82b04c3
baigsifra <84186625+baigsifra@users.noreply.github.com>
2021-10-14 12:20:03 -0500

    added comment (test)

--- app/src/main/java/com/example/moodmeter/MainActivity.java
+++ app/src/main/java/com/example/moodmeter/MainActivity.java
@@ -11,4 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // hey broski it's me, my Name is Jeff
 }
