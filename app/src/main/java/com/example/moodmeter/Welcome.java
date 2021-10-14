df02bf68e402cb522027171467fe80392c34d04c
PranPras <88902034+PranPras@users.noreply.github.com>
2021-10-14 12:34:58 -0500

    activity buttons added to welcome and home activity

--- app/src/main/java/com/example/moodmeter/Welcome.java
+++ app/src/main/java/com/example/moodmeter/Welcome.java
@@ -4,12 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Welcome extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_welcome);
     }
 
     // hey broski it's me, my Name is Jeff
