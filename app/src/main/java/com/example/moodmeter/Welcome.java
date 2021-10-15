63081dc636452b9c83d5628211ae1d410e091ef2
meganw0009 <meganhr08@gmail.com>
2021-10-15 12:46:45 -0500

    configured w/ firebase

--- app/src/main/java/com/example/moodmeter/Welcome.java
+++ app/src/main/java/com/example/moodmeter/Welcome.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class Welcome extends AppCompatActivity {
 
@@ -16,10 +17,17 @@ public class Welcome extends AppCompatActivity {
     public void toLogin(View v){
         setTitle("Log In");
         setContentView(R.layout.login);
+        EditText username = findViewById(R.id.loginUsernameET);
+        EditText password = findViewById(R.id.loginPasswordET);
     }
 
     public void toSignUp(View V){
         setTitle("Sign Up");
         setContentView(R.layout.sign_up);
+        EditText username = findViewById(R.id.signUpUsernameET);
+        EditText email = findViewById(R.id.signUpEmailET);
+        EditText password = findViewById(R.id.signUpPasswordET);
+        EditText confirmPassword = findViewById(R.id.signUpConfirmPasswordET);
+
     }
 }
