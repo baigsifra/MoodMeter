e2209e838dfd69900cca64664557f29b9b315189
AashikaLilaramani <84186621+AashikaLilaramani@users.noreply.github.com>
2021-10-26 13:12:49 -0500

    started tab layout for pet screen

--- app/src/main/java/com/example/moodmeter/Pet.java
+++ app/src/main/java/com/example/moodmeter/Pet.java
@@ -1,14 +1,26 @@
 package com.example.moodmeter;
-
+import android.widget.TabHost;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
 public class Pet extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pet);
+        TabHost tabs = (TabHost) findViewById(R.id.tabHostPet);
+        tabs.setup();
+        TabHost.TabSpec spec = tabs.newTabSpec("tag1");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("First");
+        tabs.addTab(spec);
+        spec = tabs.newTabSpec("tag2");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("second");
+        tabs.addTab(spec);
+        spec = tabs.newTabSpec("tag3");
+        spec.setContent(R.id.tab3);
+        spec.setIndicator("third");
+        tabs.addTab(spec);
     }
 }
