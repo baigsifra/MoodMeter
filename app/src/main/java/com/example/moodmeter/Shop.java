f0ce6ace32991decdb412e107681b05fe37dc28a
meganw0009 <84088878+meganw0009@users.noreply.github.com>
2021-10-26 13:13:46 -0500

    started shop functionality

--- app/src/main/java/com/example/moodmeter/Shop.java
+++ app/src/main/java/com/example/moodmeter/Shop.java
@@ -3,12 +3,63 @@ package com.example.moodmeter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 public class Shop extends AppCompatActivity {
+
+    int money = 5;
+    // get from firebase!
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
+        TextView moneyTV = findViewById(R.id.moneyTV);
+        moneyTV.setText("$" + money);
+    }
+
+    public void confirmBuy(View v){
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.buy_pop_up, null);
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = false; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, false);
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window token
+        popupWindow.showAtLocation(v, Gravity.CENTER, 0, 0);
+
+        // dismiss the popup window when touched
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
+    }
+
+    public void itemSelected(View v){
+       Button buyButton = findViewById(R.id.buyButton);
+       buyButton.setVisibility(View.VISIBLE);
+    }
+
+    public void buy(View v){
+        // add to firebase inventory
+    }
+
+    public void cancelBuy(View v){
+
     }
 }
