48a8485d52a0b1b93390841269c57535a98e7b47
PranPras <88902034+PranPras@users.noreply.github.com>
2021-10-27 12:53:33 -0500

    Added graph data

--- app/src/main/java/com/example/moodmeter/Data.java
+++ app/src/main/java/com/example/moodmeter/Data.java
@@ -4,11 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class Data extends AppCompatActivity {
+
+    GraphView graphView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data);
-    }
+
+        //source: https://www.geeksforgeeks.org/line-graph-view-in-android-with-example/
+        //initialize graph view
+        graphView = findViewById(R.id.graph);
+        // on below line we are adding data to our graph view.
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[]{
+                new DataPoint(0, 1),
+                new DataPoint(1, 3),
+                new DataPoint(3, 20),
+                new DataPoint(4, 6),
+                new DataPoint(5, 3),
+                new DataPoint(6, 6)
+        });
+        graphView.setTitle("Example Graph");
+        graphView.setTitleTextSize(35);
+        graphView.addSeries(series);
+}
 }
