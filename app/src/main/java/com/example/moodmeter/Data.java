0b4d920ef93c8e45b60578e0d1bf8e8f30f6229b
PranPras <88902034+PranPras@users.noreply.github.com>
2021-10-29 12:55:07 -0500

    scatterplot

--- app/src/main/java/com/example/moodmeter/Data.java
+++ app/src/main/java/com/example/moodmeter/Data.java
@@ -1,16 +1,22 @@
 package com.example.moodmeter;
 
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.Viewport;
+import com.jjoe64.graphview.helper.StaticLabelsFormatter;
 import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.DataPointInterface;
+import com.jjoe64.graphview.series.OnDataPointTapListener;
+import com.jjoe64.graphview.series.PointsGraphSeries;
+import com.jjoe64.graphview.series.Series;
 
 public class Data extends AppCompatActivity {
 
-    GraphView graphView;
+    GraphView logScatterPlot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,18 +25,38 @@ public class Data extends AppCompatActivity {
 
         //source: https://www.geeksforgeeks.org/line-graph-view-in-android-with-example/
         //initialize graph view
-        graphView = findViewById(R.id.graph);
+        logScatterPlot = findViewById(R.id.graph);
         // on below line we are adding data to our graph view.
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[]{
-                new DataPoint(0, 1),
-                new DataPoint(1, 3),
+        PointsGraphSeries<DataPoint> logSeries = new PointsGraphSeries<DataPoint>(new DataPoint[]{
+                new DataPoint(1, 1),
+                new DataPoint(2, 3),
                 new DataPoint(3, 20),
                 new DataPoint(4, 6),
                 new DataPoint(5, 3),
-                new DataPoint(8, 6)
+                new DataPoint(6, 6),
+                new DataPoint(7, 10)
         });
-        graphView.setTitle("Example Graph");
-        graphView.setTitleTextSize(50);
-        graphView.addSeries(series);
+        logScatterPlot.addSeries(logSeries);
+        Viewport vp = logScatterPlot.getViewport();
+        vp.setXAxisBoundsManual(true);
+        vp.setMinX(1);
+        vp.setMaxX(7);
+        vp.setYAxisBoundsManual(true);
+        vp.setMinY(0);
+        vp.setMaxY(100);
+        logScatterPlot.setTitle("Mood Graph: Weekly Summary");
+        logScatterPlot.setTitleTextSize(50);
+        
+
+        StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(logScatterPlot);
+        staticLabelsFormatter.setHorizontalLabels(new String[]{"Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7"});
+        logScatterPlot.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
+        //change instead of toast to make it show data below
+        logSeries.setOnDataPointTapListener(new OnDataPointTapListener() {
+            @Override
+            public void onTap(Series series, DataPointInterface dataPoint) {
+                Toast.makeText(Data.this, "Series1: On Data Point clicked: " + dataPoint, Toast.LENGTH_SHORT).show();
+            }
+        });
 }
 }
