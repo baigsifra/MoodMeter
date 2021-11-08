6a9b0ce8c8deeeb000bf5550c4991b76b4e4e320
Aashika Lilaramani <84186621+AashikaLilaramani@users.noreply.github.com>
2021-11-08 12:51:42 -0600

    no more tab layout :(

--- app/src/main/java/com/example/moodmeter/Pet.java
+++ app/src/main/java/com/example/moodmeter/Pet.java
@@ -1,29 +1,15 @@
 package com.example.moodmeter;
 import android.content.Intent;
 import android.view.View;
-import android.widget.TabHost;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
 public class Pet extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pet);
-        TabHost tabs = (TabHost) findViewById(R.id.tabHostPet);
-        tabs.setup();
-        TabHost.TabSpec spec = tabs.newTabSpec("Hats");
-        spec.setContent(R.id.tab1);
-        spec.setIndicator("Hats");
-        tabs.addTab(spec);
-        spec = tabs.newTabSpec("Furniture");
-        spec.setContent(R.id.tab2);
-        spec.setIndicator("Furniture");
-        tabs.addTab(spec);
-        spec = tabs.newTabSpec("Inventory");
-        spec.setContent(R.id.tab3);
-        spec.setIndicator("Inventory");
-        tabs.addTab(spec);
     }
 
     public void toStore(View v){
