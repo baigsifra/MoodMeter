1c1ea710d63bf8f1533a7dc3099273e13a4e0ffb
AashikaLilaramani <aashika.lilaramani@gmail.com>
2021-11-14 22:02:36 -0600

    petscreendone

--- app/src/main/java/com/example/moodmeter/Pet.java
+++ app/src/main/java/com/example/moodmeter/Pet.java
@@ -20,51 +20,76 @@ public class Pet extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pet);
 
-
     }
 
     public void toStore(View v){
         Intent shopIntent = new Intent(this, Shop.class);
         startActivity(shopIntent);
+
+
     }
 
     public void itemSelected(View v){
         switch(v.getId()) {
             case R.id.hat1:
                 imageBorderShow(findViewById(R.id.hat1));
+                ImageView hat1= (ImageView)findViewById(R.id.octopusPet);
+                hat1.setImageResource(R.drawable.octopusblue);
                 break;
             case R.id.hat2:
                 imageBorderShow(findViewById(R.id.hat2));
+                ImageView hat2= (ImageView)findViewById(R.id.octopusPet);
+                hat2.setImageResource(R.drawable.octopuscowboy);
                 break;
             case R.id.hat3:
                 imageBorderShow(findViewById(R.id.hat3));
+                ImageView hat3= (ImageView)findViewById(R.id.octopusPet);
+                hat3.setImageResource(R.drawable.octopuspink);
                 break;
             case R.id.hat4:
                 imageBorderShow(findViewById(R.id.hat4));
+                ImageView hat4= (ImageView)findViewById(R.id.octopusPet);
+                hat4.setImageResource(R.drawable.octopussanta);
                 break;
             case R.id.hat5:
                 imageBorderShow(findViewById(R.id.hat5));
+                ImageView hat5= (ImageView)findViewById(R.id.octopusPet);
+                hat5.setImageResource(R.drawable.octopuswitch);
                 break;
             case R.id.hat6:
                 imageBorderShow(findViewById(R.id.hat6));
+                ImageView hat6= (ImageView)findViewById(R.id.octopusPet);
+                hat6.setImageResource(R.drawable.octopusyellow);
                 break;
             case R.id.furniture1:
                 imageBorderShow(findViewById(R.id.furniture1));
+                ImageView furniture1= (ImageView)findViewById(R.id.bgpet);
+                furniture1.setImageResource(R.drawable.bed);
                 break;
             case R.id.furniture2:
                 imageBorderShow(findViewById(R.id.furniture2));
+                ImageView furniture2= (ImageView)findViewById(R.id.bgpet2);
+                furniture2.setImageResource(R.drawable.chair);
                 break;
             case R.id.furniture3:
                 imageBorderShow(findViewById(R.id.furniture3));
+                ImageView furniture3= (ImageView)findViewById(R.id.bgpet);
+                furniture3.setImageResource(R.drawable.desk1);
                 break;
             case R.id.furniture4:
                 imageBorderShow(findViewById(R.id.furniture4));
+                ImageView furniture4= (ImageView)findViewById(R.id.bgpet2);
+                furniture4.setImageResource(R.drawable.desk2);
                 break;
             case R.id.furniture5:
                 imageBorderShow(findViewById(R.id.furniture5));
+                ImageView furniture5= (ImageView)findViewById(R.id.bgpet);
+                furniture5.setImageResource(R.drawable.dresser);
                 break;
             case R.id.furniture6:
                 imageBorderShow(findViewById(R.id.furniture6));
+                ImageView furniture6= (ImageView)findViewById(R.id.bgpet2);
+                furniture6.setImageResource(R.drawable.sofa);
                 break;
         }
     }
