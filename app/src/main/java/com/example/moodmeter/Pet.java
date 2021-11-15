5af544eb3e97f05a78bde40e2ded4647831ecb83
meganw0009 <meganhr08@gmail.com>
2021-11-15 15:16:36 -0600

    update

--- app/src/main/java/com/example/moodmeter/Pet.java
+++ app/src/main/java/com/example/moodmeter/Pet.java
@@ -68,7 +68,7 @@ public class Pet extends AppCompatActivity {
                 furniture = (ArrayList<Integer>) data.get("furniture");
                 backgrounds = (ArrayList<Integer>) data.get("backgrounds");
                 Log.i("megan", "hats before gotInventory" + hats);
-                gotInventory(hats);
+                gotInventory(data);
             }
         }, firebaseUser.getEmail());
 
@@ -81,29 +81,20 @@ public class Pet extends AppCompatActivity {
 
     }
 
-    public void gotInventory(ArrayList<Integer> items) {
+    public void gotInventory(Map<String, Object> items) {
 
         Log.i("megan", "hats in gotInventory" + hats);
-        Log.i("megan", "size " + items.size());
-        for(int i = 0; i < items.size(); i++) {
-            if (items.get(i) == (1)) {
-                findViewById(R.id.hat1).setVisibility(View.VISIBLE);
-            } else if (items.get(i) == 2) {
-                findViewById(R.id.hat2).setVisibility(View.VISIBLE);
-            } else if (items.get(i) == 3) {
-                findViewById(R.id.hat3).setVisibility(View.VISIBLE);
-            } else if (items.get(i) == 4) {
-                findViewById(R.id.hat4).setVisibility(View.VISIBLE);
-            } else if (items.get(i) == 5) {
-                findViewById(R.id.hat5).setVisibility(View.VISIBLE);
-            } else if (items.get(i) == 6) {
-                findViewById(R.id.hat6).setVisibility(View.VISIBLE);
+
+        for(int i = 0; i <= 2; i++){
+            if (i == 0){
+                for(int i = 0, i < (ArrayList<Integer>)items.get(0).size(); i++){
+                    if ((ArrayList<Integer>)items.get(0).get(i) == (1)) {
+                        findViewById(R.id.hat1).setVisibility(View.VISIBLE);
+                    }
+                }
             }
         }
-        if (items.get(0) == (1)) {
-                findViewById(R.id.hat1).setVisibility(View.VISIBLE);
 
-        }
     }
 
     public void checkOwnership(ArrayList<Integer> items){
